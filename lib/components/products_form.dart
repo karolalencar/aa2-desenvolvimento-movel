c7dfab2418da3ed7434018b4246d1ffe4fab2269
emiliasousa <emiliasousa@estudante.ufscar.br>
2023-08-29 21:08:58 -0300

    Fix form

--- lib/components/products_form.dart
+++ lib/components/products_form.dart
@@ -15,27 +15,86 @@ class _ProductsFormState extends State<ProductsForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        // child: Column(children: [
-        //   Form(
-        //       child: Scaffold(
-        //     body: Center(
-        //         child: Column(
-        //       children: [
-        //         TextFormField(
-        //           validator: (String? value) {
-        //             if (value != null && value.isEmpty) {
-        //               return 'Insira o nome do produto.';
-        //             }
-        //             return null;
-        //           },
-        //           controller: nameController,
-        //           textAlign: TextAlign.center,
-        //         ),
-        //       ],
-        //     )),
-        //   ))
-        // ]),
-        );
+    return Scaffold(
+      //backgroundColor: Colors.green, // Plano de fundo verde
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [const Color.fromARGB(255, 247, 249, 250), Color.fromARGB(255, 190, 250, 189)], // Cores do gradiente
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.add_shopping_cart_outlined , // Ícone centralizado
+              size: 100,
+              color: Color.fromARGB(255, 19, 131, 23),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Nome do Produto',
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: 'Codigo',
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: 'Quantidade ',
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: 'Descrição',
+                  fillColor: Colors.white,
+                  filled: true,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Lógica para o botão de submit
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Color.fromARGB(255, 19, 131, 23), // Background botao
+              ),              
+              child: Text('Cadastrar'),
+            ),
+          ],
+        ),
+      ),
+      ),
+    );
   }
 }
